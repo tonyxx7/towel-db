c30b403b47e2aa148fa56d431510b53ecf593d16
i80and <i80and@02267986-114a-0410-a1bc-4b731ce5efbb>
2008-04-04 20:02:01 +0000

    Initial upload.

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+setup(	name = 'towel-db',
+		version = '0.8',
+		author = 'Andrew Aldridge',
+		author_email = 'i80and@gmail.com',
+		description = 'A simple, textual, database library',
+		py_modules = ['towel-db'],
+		)
---
+++ toweldb.py
@@ -0,0 +1,300 @@
+# towel-db - A human readable database system.
+# Copyright (C) 2008  Andrew Aldridge <i80and@gmail.com>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import os
+import string
+import __builtin__
+
+_open = __builtin__.open
+
+# Version of the database
+VERSION = 0
+
+# Reserved record names
+_DB_META = "_db"
+DB_RESERVED = [_DB_META]
+
+# Database open flags
+DB_READ = "r"
+DB_WRITE = "w"
+DB_CREATE = "c"
+
+class DBError( Exception ):
+	"""Generic database error"""
+
+class DBVersionMismatch( DBError ):
+	"""Version of database is different from the version of the towel-db
+	library being used"""
+
+class DBWrongMode( DBError ):
+	"Incorrect mode for the requested operation"
+
+class DBNotFound( DBError ):
+	"No valid database was found"
+
+class _Record:
+	"A record within a towel-db database"
+	path = ""
+	exists = False
+	mode = ""
+	key = ""
+	changed = False
+	data = {}
+
+	def __init__( self, path, key, mode ):
+		"""Create a record object
+		Takes a path string and a key string as an argument"""
+		self.path = path
+		self.key = key
+		self.mode = mode
+		self.data = {}
+		changed = False
+		
+		record_path = os.path.join( path, key )
+
+		# Figure out if the record exists
+		if os.path.exists( record_path ):
+			self.exists = True
+
+			# Open the record
+			recfile = _open( record_path, "r" )
+			self._read( recfile.read( ))
+			recfile.close()
+	
+	def __len__( self ):
+		return len( self.data )
+
+	def __getitem__( self, key ):
+		try:
+			return self.data[key]
+		except( KeyError ):
+			raise KeyError, "Field not found in record"
+
+	def __iter__( self ):
+		return iter( self.data )
+
+	def __setitem__( self, key, value ):
+		# Check mode
+		if self.mode is DB_READ:
+			raise DBWrongMode, "Database opened read-only"
+		
+		# Make sure the type is right
+		if not type( key ) == type( value ) == str:
+			raise TypeError, "Key and value must both be strings (for now)"
+		if not self.changed:
+			# Record has chanegd; OK to sync
+			self.changed = True
+		
+		self.data[key] = value
+		
+	def __delitem__( self, key ):
+		if self.mode is DB_READ:
+			raise DBWrongMode, "Database opened read-only"
+		
+		if key in self.data:
+			del self.data[key]
+			self.changed = True
+		else:
+			raise KeyError, "Given key is not in record"
+
+	def __del__( self ):
+		if self.changed:
+			self.sync()
+
+	def _read( self, data ):
+		"""Read the database record.
+		Takes a data string as an argument"""
+
+		current_column = ""
+
+		# FIXME: Allow for escaping "%%"
+		# Parse the data
+		for line in data.splitlines( ):
+			if line[0:2] == '%%':
+				# Header line
+				current_column = line[2:]
+				self.data[current_column] = []
+			else:
+				# Data line
+				self.data[current_column].append( line )
+
+		# Concatenate data lines
+		for field in self.data:
+			# Iterate through all but the last line
+			for line_index in xrange( len( self.data[field] ) - 1 ):
+				self.data[field].insert( line_index+1, '\n' )
+			self.data[field] = string.join( self.data[field], "" )
+
+	def change_key( self, key, clear=True ):
+		"""Mutator function to change the record key.
+		change_key( key, clear=True )"""
+		if not type( key ) == str:
+			raise TypeError, "Key must be a string"
+		
+		self.key = key
+		if clear:
+			self.data = {}
+		
+		if not self.changed:
+			self.changed = True
+		
+	def get_key( self ):
+		"Accessor function to get the key of the record"
+		return self.key
+
+	def clear( self ):
+		"Clear the record of data"
+		self.data = {}
+		changed = True
+
+	def sync( self ):
+		data_t = []
+		data = ""
+		
+		record_path = os.path.join( self.path, self.key )
+
+		# Open the file
+		datafile = _open( record_path, "w" )
+
+		if len( self.data ):
+
+			# Construct the data
+			for field in self.data.keys():
+				data_t.append( '%%' )
+				data_t.append( field )
+				data_t.append( '\n' )
+				data_t.append( self.data[field] )
+				data_t.append( '\n' )
+
+			# Join and clean the data
+			data_t.pop()
+			data = string.join( data_t, "" )
+			
+
+		# Write the data
+		datafile.write( data )
+
+		# Close the file
+		datafile.close( )
+		
+		# No changes to be flushed now
+		self.changed = False
+
+class _db:
+	"A human-readable and fairly simple database system"
+	path = ""
+	mode = ""
+	
+	def __init__( self, path, mode="DB_READ" ):
+		""" Create an towel-db database
+		Takes a path to a database directory tree as an argument"""
+		self.path = path
+		self.mode = mode
+		
+		# Make sure that the path exists
+		if not os.path.isdir( self.path ):
+			if self.mode != DB_CREATE:
+				raise DBNotFound, "No database exists"
+			else:
+				self._create_db()
+		
+		# Make sure that this is a database, and check the version
+		try:
+			meta = self[_DB_META]
+		except( KeyError ):
+			if self.mode != DB_CREATE:
+				raise DBNotFound, "Not a valid database"
+			else:
+				self._create_db()
+				del meta
+		
+		if int( self[_DB_META]['version'] ) != VERSION:
+			raise DBVersionMismatch, "Database version mismatch"
+
+	def _create_db( self ):
+		"""Create a database"""
+
+		# Check the mode
+		if self.mode != DB_CREATE:
+			raise DBWrongMode, "'Create' mode not set"
+		
+		# Check if we need to create the directory
+		if not os.path.isdir( self.path ):
+			os.mkdir( self.path )
+		
+		# Create the database meta file
+		meta = _Record( self.path, _DB_META, self.mode )
+		meta["version"] = str( VERSION )
+		meta.sync()
+		
+	def __str__( self ):
+		return string.join( ["towel-db database at", self.path] )
+
+	def __len__( self ):
+		return len( os.listdir( self.path ))
+
+	def __getitem__( self, key ):
+		"""Get a record from the key given"""
+
+		# Make sure the key exists
+		if not( os.path.exists( os.path.join( self.path, key ))):
+			raise KeyError, key
+
+		# Get the data file
+		record = _Record( self.path, key, self.mode )
+
+		return record
+		
+	def __iter__( self ):
+		return iter( os.listdir( self.path ))
+	
+	def __setitem__( self, key, value ):
+			if not type( key ) == str:
+				raise TypeError, "Key must be a string"
+			
+			if key in DB_RESERVED:
+				raise DBError, "Requested key is reserved"
+			
+			if self.mode == DB_READ:
+				raise DBWrongMode, "Database opened read-only"
+			
+			# Assign
+			if isinstance( value, _Record ):
+				record = value
+				record.change_key( key, False )
+				record.sync()
+			elif value is None:
+				record = _Record( self.path, key, self.mode )
+				record.sync()
+			else:
+				raise TypeError, "Value must be a record (for now)"
+
+	def __delitem__( self, key ):
+		if self.mode == DB_READ:
+			raise DBWrongMode, "Database opened read-only"
+			
+		os.remove( os.path.join( self.path, key ))
+		
+	def create( self, key ):
+		"Create an empty new database record.  Takes a key string"
+		if not type( key ) == str:
+			raise TypeError, "Key must be a string"
+			
+		record = _Record( self.path, key, self.mode )
+		record.sync()
+
+def open( path, mode=DB_READ ):
+	return _db( path, mode )
