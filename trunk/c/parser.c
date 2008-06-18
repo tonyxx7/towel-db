6c983be2829a60d6e3dbd79d78fa21f30b1c37d6
i80and <i80and@02267986-114a-0410-a1bc-4b731ce5efbb>
2008-06-18 01:52:35 +0000

    Oops; getting the end of the token actually makes more sense.

--- trunk/c/parser.c
+++ trunk/c/parser.c
@@ -98,10 +98,8 @@ toweldb_read_rec( toweldb_db* db, const char* key )
 				cur_node->next = NULL;
 			}
 			
-			/* Mark the location of the end of the token.  We have to subtract
-			 * the length of our token to get the start of it instead of the
-			 * end. */
-			cur_node->key_loc = ( current_char - TOWELDB_PHASE_FINISH );
+			/* Mark the location of the end of the token */
+			cur_node->key_loc = current_char;
 			
 			/* It's official: we are in a key */
 			rec_component = TOWELDB_COMPONENT_KEY;
@@ -135,6 +133,7 @@ toweldb_read_rec( toweldb_db* db, const char* key )
 		if( rec_component == TOWELDB_COMPONENT_KEY )
 		{
 			cur_node->key_len++;
+			printf( "%c\n", rec_contents[current_char] );
 		}
 		else if( rec_component == TOWELDB_COMPONENT_DATA )
 		{
